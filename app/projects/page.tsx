94aebb0d76c5b062702ae0933845dc3469cedbb3
Guilecompo <kideguilecompo03@gmail.com>
2026-01-30 20:19:15 +0800

    zcx

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -193,9 +193,7 @@ export default function ProjectPage() {
                         src={project.src}
                         alt={project.title}
                         fill
-                        sizes="(max-width: 640px) 100vw,
-         (max-width: 1024px) 50vw,
-         33vw"
+                        sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 33vw"
                         className="object-cover transition-transform duration-700 group-hover:scale-110"
                       />
 
